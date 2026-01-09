6c3efca87c704e8f3726e4a07860d61625388489
is <leo.bjorkebaum@elev.ga.lbs.se>
2026-01-09 12:09:55 +0100

    börjat på skottets rörelse

---
+++ Programveckor-grupp-1-5/Assets/bullet.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class bullet : MonoBehaviour
+{
+    Rigidbody2D rigidbody;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody2D>();
+        rigidbody.linearVelocity = new Vector2(3, 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
