053bf49965a6e2a00ac8a071d465fc4f50fd7a6b
is <leo.bjorkebaum@elev.ga.lbs.se>
2026-01-12 09:53:02 +0100

    scene part 2

--- Programveckor-grupp-1-5/Assets/scripts/movement.cs
+++ Programveckor-grupp-1-5/Assets/scripts/movement.cs
@@ -22,14 +22,7 @@ public class movement : MonoBehaviour
         {
             rb.linearVelocity = new Vector2(0, -5);
         }
-        if (Input.GetKey(KeyCode.RightArrow))
-        {
-            rb.linearVelocity = new Vector2(5, 0);
-        }
-        if (Input.GetKey(KeyCode.LeftArrow))
-        {
-            rb.linearVelocity = new Vector2(-5, 0);
-        }
+
         //flyttar med hjälp av wasd
         if (Input.GetKey(KeyCode.W))
         {
@@ -39,13 +32,7 @@ public class movement : MonoBehaviour
         {
             rb.linearVelocity = new Vector2(0, -5);
         }
-        if (Input.GetKey(KeyCode.D))
-        {
-            rb.linearVelocity = new Vector2(5, 0);
-        }
-        if (Input.GetKey(KeyCode.A))
-        {
-            rb.linearVelocity = new Vector2(-5, 0);
-        }
+
+
     }
 }
