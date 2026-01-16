d27ff82a6eaf859b9c364d0422927dc313ab8f30
is <leo.bjorkebaum@elev.ga.lbs.se>
2026-01-16 14:05:55 +0100

    fix

---
+++ Programveckor-grupp-1-5/Assets/bak3.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+public class bak3 : MonoBehaviour
+{
+
+    Rigidbody2D rigidbody;
+    SpriteRenderer sr;
+    Animator animator;
+    float score1 = 0;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody2D>();
+        rigidbody.linearVelocity = new Vector2(-5, 0);
+        animator = GetComponent<Animator>();
+        sr = GetComponent<SpriteRenderer>();
+        animator.Play("Mushroom distortion 6");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position.x < -91)
+        {
+            transform.position = new Vector2(90, -5);
+        }
+    }
+}
